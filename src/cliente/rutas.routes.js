2798dce05b911d7d72e38e6110a85b56c1bfb330
nico1612 <nicogonzalezraggi123@hotmail.com>
2023-02-06 11:29:12 -0300

    manejo de algunos errores

--- src/cliente/rutas.routes.js
+++ src/cliente/rutas.routes.js
@@ -3,9 +3,11 @@ const { getUsers, getUsersXId} = require("./userController")
 const router = express.Router()
 
 router.get("/usuarios",getUsers)
+router.get("/usuarios/",getUsers)
 
 //obtengo usuarios por id 
 router.get("/usuario/:id",getUsersXId)
 router.get("/usuario/",getUsersXId)
+router.get("/usuarios/:id",getUsersXId)
 
 module.exports = router;

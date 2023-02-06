fc28cde61f4b5f10979999323502a128981c5108
nico1612 <nicogonzalezraggi123@hotmail.com>
2023-02-06 10:46:57 -0300

    formato de la respuesta usuarios

--- src/cliente/dbUsuarios.js
+++ src/cliente/dbUsuarios.js
@@ -3,17 +3,18 @@ const sql = require('mssql');
 
 async function getUsuarios(){
     try{
-        console.log("hola")
         let pool = await sql.connect(config);
-        let usuarios = await pool.request().query("select usuarios.nombre,"+
-        "usuarios.apellido, usuarios.edad, empresa.nombre_empresa,"+
-        "descripcion.desripcion,"+
-        "descripcion.años_trabajados"+
-        " from usuarios"+
-        " join ((empleo join empresa on empleo.id_empresa=empresa.id)"+
-        "join descripcion on"+
-        " empleo.id_descripcion_Empleo=descripcion.id_descripcion)"+
-        "on usuarios.id=empleo.id_usuario");
+        let usuarios = await pool.request()
+        .query("SELECT usuarios.nombre,"+
+        "usuarios.apellido,usuarios.edad,"+
+        "string_agg(empresa.nombre_empresa,','),"+
+        "string_agg(descripcion.desripcion, ','),"+
+        "string_agg(descripcion.años_trabajados , ',')"+
+        "FROM (usuarios join"+
+        "((empleo join empresa ON empleo.id_empresa=empresa.id)"+
+        "JOIN descripcion ON empleo.id_descripcion_Empleo=descripcion.id_descripcion)"+
+        "ON usuarios.id=empleo.id_usuario)"+
+        "group by usuarios.nombre,usuarios.apellido,usuarios.edad");
         return usuarios.recordsets;
     }
     catch(error){
